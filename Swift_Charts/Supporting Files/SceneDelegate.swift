d8d5c20f310cf50ffc8eaf35e241f5013396e001
Dawei Hao <dwsamurai84@gmail.com>
2025-01-19 17:32:51 +0800

    Add sceneDelegate.

--- Swift_Charts/Supporting Files/SceneDelegate.swift
+++ Swift_Charts/Supporting Files/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScence = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScence)
-        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.rootViewController = UINavigationController(rootViewController: HomeController())
         self.window = window
         self.window?.makeKeyAndVisible()
     }
